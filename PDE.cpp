f30c6ad8c08c05d90f5e88a8fb88b810e95a96df
JuanPuerto <42391472+JuanPuerto@users.noreply.github.com>
2018-11-17 17:39:19 -0500

    Add files via upload

---
+++ PDE.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define k 1.62
+#define Cp 820
+#define rho 2.71
+#define T_CALCITA 10.0
+#define T_VARILLA 100.0
+
+double nu = double(k/(Cp*rho));
+
+int main(void)
+{
+	
+	
+	return 0;
+}
