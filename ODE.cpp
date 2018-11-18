41dba50ed1e3cad8a1cd3d181219fef558e61679
JuanPuerto <42391472+JuanPuerto@users.noreply.github.com>
2018-11-18 18:50:51 -0500

    Update ODE.cpp

--- ODE.cpp
+++ ODE.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <math.h>
+#include <stdio.h>
+#include <fstream>
 #include <stdlib.h>
 
-#define PI 3.14159
+#define pi 3.14159	//definir constante pi
+#define g 10.0	//aceleracion gravitacional en m*s⁻²
+#define c 0.2	//coeficiente de friccion sin unidades
+#define m 0.2	//masa del proyectil en kg
+#define dt 0.0001	//avance en el tiempo
+#define N 21000	//espaciamiento de lugar
+
+#define for_i1 for(int i=0;i<N;i++)	//loop para todos los valores de los arreglos
+#define for_i2 for(int i=1;i<N;i++)	//loop para valores internos de los arreglos
+
+double x_dir[N], y_dir[N], vel_x[N], vel_y[N], t[N];	//arreglos necesarios para avanzar
 
 using namespace std;
 
