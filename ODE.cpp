07d1ab791a15efb65bf87bee6775cc5dc6ac2120
JuanPuerto <42391472+JuanPuerto@users.noreply.github.com>
2018-11-18 20:04:38 -0500

    Update ODE.cpp

--- ODE.cpp
+++ ODE.cpp
@@ -11,10 +11,15 @@
 #define dt 0.0001	//avance en el tiempo
 #define N 21000	//espaciamiento de lugar
 
-#define for_i1 for(int i=0;i<N;i++)	//loop para todos los valores de los arreglos
-#define for_i2 for(int i=1;i<N;i++)	//loop para valores internos de los arreglos
+#define for_i0 for(int i=0;i<N;i++)	//loop para todos los valores de los arreglos
+#define for_i1 for(int i=1;i<N;i++)	//loop para valores internos de los arreglos
 
 double x_dir[N], y_dir[N], vel_x[N], vel_y[N], t[N];	//arreglos necesarios para avanzar
+
+double prim_derx(double);
+double seg_derx(double,double);
+double prim_dery(double);
+double seg_dery(double,double);
 
 using namespace std;
 
@@ -23,4 +28,24 @@ int main()
 	
 	
 	return 0;
+}
+
+double prim_derx(double vel_x)
+{
+	return vel_x;
+}
+
+double seg_derx(double vel_x,double vel_y)
+{
+	return -(c/m)*vel_x*sqrt((vel_x*vel_x)+(vel_y*vel_y));
+}
+
+double prim_dery(double vel_y)
+{
+	return vel_y;
+}
+
+double seg_dery(double vel_x,double vel_y)
+{
+	return -g-(c/m)*vel_y*sqrt((vel_x*vel_x)+(vel_y*vel_y));
 }
